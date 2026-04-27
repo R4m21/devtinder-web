4703b559769a3d7a325db767bdebc9e6b7b1a7d1
mani <cr4m21@gmail.com>
2026-04-27 06:30:02 +0530

    chore: env file variable 1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = process.env.VITE_BASE_URL || "http://localhost:8000";
+export const BASE_URL =
+  import.meta.env.VITE_BASE_URL || "http://localhost:8000";
 
 export const STATUS_MAP = {
   ignore: "ignored",
